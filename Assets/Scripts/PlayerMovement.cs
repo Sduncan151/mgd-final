d65db0a7f062b39b2eb1406b45dbe16c905a9b4d
Sydney <sydneyjduncan@gmail.com>
2021-10-27 10:50:06 -0500

    Jumps half-fixed, player fixed, score fixed

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,25 +8,31 @@ public class PlayerMovement : MonoBehaviour
     [Range(5f,50f)]
     public float speed = 10, jumpForce = 5;
 
+    public int coinValue = 1;
+
     public Vector3 dir;
     public Vector3 startPosition;
 
     public bool isGrounded = true;
+    public bool canJump = false;
 
-    int coins = 0;
+    private Rigidbody rb;
+    private int coins = 0;
 
-    // get a reference to a rigidbody.
-    Rigidbody rb;
-
-    void Awake()
-    {
-        DontDestroyOnLoad(this.gameObject);
-    }
+    // void Awake()
+    // {
+    //     DontDestroyOnLoad(this.gameObject);
+    // }
 
     void Start()
     {
         rb = this.GetComponent<Rigidbody>();
         startPosition = GameObject.Find("Start Here").transform.position;
+        if(PlayerPrefs.GetInt("canJump") == 1)
+        {
+            Debug.Log("We can jump")
+            canJump = true;
+        }
         ResetPlayer();
     }
 
@@ -46,11 +52,13 @@ public class PlayerMovement : MonoBehaviour
         rb.velocity = Vector3.zero;                     // set speed to zero
         rb.angularVelocity = Vector3.zero;              // set rotation to zero
         this.transform.rotation = Quaternion.identity;  // set rotation to 0,0,0
+
+        // canJump = false;
     }
 
     public void Jump()
     {
-        if(isGrounded) 
+        if(isGrounded && canJump) 
         {
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
@@ -67,6 +75,14 @@ public class PlayerMovement : MonoBehaviour
         {
             Destroy(other.gameObject);
             coins++;
+            ScoreManager.instance.ChangeScore(coinValue);
+        }
+
+        else if(other.gameObject.CompareTag("JumpPowerUp"))
+        {
+            canJump = true;
+            PlayerPrefs.SetInt("canJump", 1);   // 1 is true, 0 is false.
+            Destroy(other.gameObject);
         }
     }
 
